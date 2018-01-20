f6f7c23b98d26a0df0239f90dc4a3c0039f06cca
jachqaz <jach_1997@hotmail.com>
2018-01-19 23:17:11 -0500

    Se completa para que guarde todos los campos en Firebase (solo guarda un registro), y falta varios elementos.

--- public/js/register_request/register.js
+++ public/js/register_request/register.js
@@ -5,17 +5,41 @@ $(() => {
     function sendInformationButton() {
         let time = $("#selectTime").val();
         let technologyDev = $("#selectTechnology").val();
+        let resource = $("#selectResource").val();
+        let nemployees = $("#selectNemployees").val();
+        let size = $("#selectSize").val();
+        let ubication = $("#selectUbication").val();
+        let country = $("#selectCountry").val();
         let name = $("#name").val();
+        let lastname = $("#lastname").val();
+        let email = $("#email").val();
+        let company = $("#company").val();
+        let wichTecnology = $("#wichTecnology").val();
+        let needs = $("#needs").val();
+        let salary = $("#salary").val();
+        let benefits = $("#benefits").val();
 
         let requestDev = new RequestDeveloper();
 
         requestDev.save({
             time,
             technologyDev,
-            name
+            resource,
+            nemployees,
+            size,
+            ubication,
+            country,
+            name,
+            lastname,
+            email,
+            company,
+            wichTecnology,
+            needs,
+            salary,
+            benefits
         });
 
-        
+
     }
 
 
